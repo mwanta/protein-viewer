78948ca9ad3f69ad3f9c18f8c24119a9787d4867
Meredith Wanta <wanta.m@northeastern.edu>
2026-03-04 11:11:05 -0500

    initial-commit

---
+++ src/main/java/com/meredithwanta/protein_viewer/ProteinViewerApplication.java
@@ -0,0 +1,13 @@
+package com.meredithwanta.protein_viewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProteinViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProteinViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meredithwanta/protein_viewer/ProteinViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meredithwanta.protein_viewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProteinViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
