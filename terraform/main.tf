3f332304468ce9ff6a1104f19e3338a658b9f06a
Meredith Wanta <wanta.m@northeastern.edu>
2026-03-18 15:50:02 -0400

    auto-terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "protein-viewer-tfstate-863852787491"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 # Configure the AWS provider
